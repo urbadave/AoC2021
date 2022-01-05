bba291680b84fd2a3d50697c75a033425aea2261
David Urbanek <david.urbanek@aruplab.com>
2022-01-05 15:55:39 -0700

    Working on a generic worker service based console app

---
+++ Day01/Day01/Program.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System;
+using System.Threading.Tasks;
+
+namespace Day01
+{
+    class Program
+    {
+        static Task Main(string[] args)
+        {
+            using var host = CreateHostBuilder(args);
+            return host.RunAsync();
+        }
+
+        private static IHost CreateHostBuilder(string[] args)
+        {
+            var defaultBuilder = Host.CreateDefaultBuilder(args)
+                .ConfigureServices((hbContext, services) => services.AddHostedService<Worker>())
+                .Build();
+            return defaultBuilder;
+        }
+    }
+}
---
+++ Day01/Day01/Worker.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Extensions.Hosting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Day01
+{
+    public class Worker : BackgroundService
+    {
+        protected override Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            Console.WriteLine("Hello World!");
+            return base.StopAsync(stoppingToken);
+        }
+    }
+}
